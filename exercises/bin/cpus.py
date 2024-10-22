f62387b4fa0b8181222ea44447d068f4f545e45e
Emmanueleon <emmanuel.cervam@gmail.com>
2024-10-21 20:30:15 -0600

    Change format

--- exercises/bin/cpus.py
+++ exercises/bin/cpus.py
@@ -1,4 +1,5 @@
-import os # importar la librería os
+import os  # importar la librería os
 
-cpu_num = os.cpu_count() # obtener el número de CPU en el sistema
-print(f"Mi computadora tiene {cpu_num} núcleos de CPU") # imprimir el número de CPU
+cpu_num = os.cpu_count()  # obtener el número de CPU en el sistema
+# imprimir el número de CPU
+print(f"Mi computadora tiene {cpu_num} núcleos de CPU")
