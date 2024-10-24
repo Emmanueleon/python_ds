e3f3faf7cd39f9d33abe46307b13b7db2e3cc3cd
Emmanueleon <emmanuel.cervam@gmail.com>
2024-10-23 23:43:20 -0600

    Tune spotify_popularity_app code

--- projects/web_development/bin/spotify_popularity_app.py
+++ projects/web_development/bin/spotify_popularity_app.py
@@ -15,14 +15,13 @@ st.header("Proyecto sprint 6")
 # Botones del programa
 
 # Botón histograma ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-st.header("Histograma de la popularidad")
-
-# crear un botón
+# Crear el botón
 if st.button("Construir histograma para tu conjunto de datos"):
 
     # Escribe un mensaje interactivo
     st.write("Creando histrograma...")
 
+    # Validar que la columna exista
     if "track_popularity" in spotify_df.columns:
 
         # Crea el histograma
@@ -30,7 +29,7 @@ if st.button("Construir histograma para tu conjunto de datos"):
                                 x="track_popularity",
                                 title="Distribución de la Popularidad de las Canciones")
 
-        # mostrar un gráfico Plotly interactivo
+        # Mostrar un gráfico Plotly interactivo
         st.plotly_chart(hist_fig, use_container_width=True)
 
     else:
@@ -39,23 +38,29 @@ if st.button("Construir histograma para tu conjunto de datos"):
 
 
 # Botón gráfico dispersión ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-# st.header("Botón gráfico dispersión")
 
-# build_scatter= st.checkbox('Construir un scatter')
-# if build_scatter:
-#    # escribir un mensaje
-#    st.write('Creación de un scatter....')
-#
-    # crear un histograma
-#    scatter_fig = px.scatter(spotify_df,
-#                             x="energy",
-#                             y="track_popularity",
-#                             color="playlist_genre",
-#                             facet_col="playlist_genre",
-#                             opacity=0.2,
-#                             title="Relación popularidad y la energía por género",
-#                            labels={"energy": "Energía", "track_popularity": "Popularidad de la canción"},
-#                             trendline="ols")
-#
-    # mostrar un gráfico Plotly interactivo
-#    st.plotly_chart(scatter_fig, use_container_width=True)
+# Crear el checkbox
+if st.checkbox("Construir el gráfico de dispersión para tu conjunto de datos")
+
+  # Escribe un mensaje interactivo
+  st.write('Creando gráfico de dispersión....')
+
+   # Validar que la columna exista
+   if "energy" in spotify_df.columns and "track_popularity" in spotify_df.columns and "playlist_genre" in spotify_df.columns:
+
+        # Crea el scatterplot
+        scatter_fig = px.scatter(spotify_df,
+                                 x="energy", y="track_popularity",
+                                 color="playlist_genre", opacity=0.2,
+                                 facet_col="playlist_genre",
+                                 title="Relación popularidad y la energía por género",
+                                 labels={
+                                     "energy": "Energía", "track_popularity": "Popularidad de la canción"},
+                                 trendline="ols")
+
+        # Mostrar un gráfico Plotly interactivo
+        st.plotly_chart(scatter_fig, use_container_width=True)
+
+    else:
+        st.error(
+            "No se encuentran las columnas para crear el gráfico de dispersión.")
